5e9757f31600b6eae05a2cbc1655f42433bded30
Jonny Dailey <jonndailey@gmail.com>
2018-05-15 09:08:49 -0400

    Removed the comment and changed the directory to a comment

--- mv.py
+++ mv.py
@@ -2,9 +2,9 @@ import os
 import shutil
 
 def main():
-	for filename in os.listdir("/home/jonny/Desktop/testing"):
+	for filename in os.listdir('''directory'''):
 		if filename:
-			newname = os.path.splitext(filename)[0]
-			new_directory = os.mkdir(newname) #make directory 
-			shutil.copy(filename,new_directory)
+			newname = os.path.splitext(filename)[0] 
+			new_directory = os.mkdir(newname) 
+			shutil.copy(filename,new_directory) 
 main()
